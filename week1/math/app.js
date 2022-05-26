3ab433874ddeca9673251bb91581887cb9f7a238
SlicedorDiced <Joshua.luna-perez@bryanuniversity.edu>
2022-05-26 13:01:44 -0400

    week1

---
+++ week1/math/app.js
@@ -0,0 +1,11 @@
+const {add,subtract,multiply,divide} = require('./math');
+
+const adding = add(200,500);
+
+const sub = subtract(50,20);
+
+const mul = multiply(10,3);
+
+const div = divide(60,2);
+
+console.log(adding,sub,mul,div);
